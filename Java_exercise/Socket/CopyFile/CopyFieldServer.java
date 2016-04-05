5f7be8c25c15392b4e64f48b0e9161ddce964517
zengge95 <sweetifan@gmail.com>
2016-04-05 22:32:18 +0800

    add a exercise

---
+++ Java_exercise/Socket/CopyFile/CopyFieldServer.java
@@ -0,0 +1,44 @@
+package CopyFile;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ * Created by Administrator on 2016/4/5.
+ */
+public class CopyFieldServer {
+    public static void main(String [] agrs){
+
+    }
+}
+class Server{
+    public static final int SERVER_PORT = 8888;
+    public Server(){
+        try {
+            ServerSocket ss = new ServerSocket(SERVER_PORT);
+            Socket s = ss.accept();
+
+            InputStream in =s.getInputStream();
+            FileOutputStream fo = new FileOutputStream("D:\\hehe.txt");
+
+            byte[] b = new byte[1024];
+            int len = -1;
+            while (true){
+                len  = in.read(b);
+
+                if (len == -1){
+                    break;
+                }
+                fo.write(b,0,len);
+            }
+            fo.close();
+            in.close();
+            s.close();
+            ss.close();
+        }catch (UnknownHostException ue){
+            ue.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
