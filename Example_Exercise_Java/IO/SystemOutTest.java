253b65f7610d4168ae1a1209115d6e3339dc9eab
zengge95 <sweetifan@gmail.com>
2016-04-09 17:32:41 +0800

    add a exercise

---
+++ Example_Exercise_Java/IO/SystemOutTest.java
@@ -0,0 +1,15 @@
+package IO;
+
+import java.io.OutputStream;
+
+/**
+ * System.out实现简单的输出,System.out返回的是OutputStream 类型的方法
+ * System.out是OutputStream的子类，由系统默认提供好的实例化对象
+ * Created by zeng on 16-4-9.
+ */
+public class SystemOutTest {
+    public static void main(String[] args)throws Exception {
+        OutputStream out = System.out;
+        out.write("hello world".getBytes()); //能根据传进去的子类的不同而输出
+    }
+}
