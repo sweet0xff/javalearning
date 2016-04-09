44953acf4947fa4583e5cde27b17ccf62f68c6e8
zengge95 <sweetifan@gmail.com>
2016-04-09 17:30:20 +0800

    add a exercise

---
+++ Example_Exercise_Java/IO/BufferedReader.java
@@ -0,0 +1,13 @@
+package IO;
+
+/**
+ * 字符缓冲区流：BufferedReader,BufferedWriter
+ * 字符缓冲区流：BufferedInputStream,BufferedOutputStream
+ * 重点方法 public string readLine()
+ * Created by zeng on 16-4-9.
+ */
+public class BufferedReader {
+    public static void main(String[] args) {
+
+    }
+}
