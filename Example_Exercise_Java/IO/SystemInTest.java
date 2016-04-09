50f52044ad91bf6c095177a35ff97de5330d0575
zengge95 <sweetifan@gmail.com>
2016-04-09 18:08:15 +0800

    fix a bug

--- Example_Exercise_Java/IO/SystemInTest.java
+++ Example_Exercise_Java/IO/SystemInTest.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.sql.SQLOutput;
 
 /**
- * System实例化对象使用的事PrintStream类对象进行的输出设备显示.
+ * System实例化对象使用的是PrintStream类对象进行的输出设备显示.
  * Created by zeng on 16-4-9.
  */
 public class SystemInTest {
