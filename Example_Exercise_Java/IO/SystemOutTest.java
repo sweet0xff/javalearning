c3b471c03bad93c3bd241bf5fe089c73b8c64b03
Ge Zeng <sweetifan@gmail.com>
2016-04-09 17:58:12 +0800

    Update SystemOutTest.java

--- Example_Exercise_Java/IO/SystemOutTest.java
+++ Example_Exercise_Java/IO/SystemOutTest.java
@@ -4,7 +4,7 @@ import java.io.OutputStream;
 
 /**
  * System.out实现简单的输出,System.out返回的是OutputStream 类型的方法
- * System.out是OutputStream的子类，由系统默认提供好的实例化对象
+ * System.out是由系统默认提供好的实例化对象
  * Created by zeng on 16-4-9.
  */
 public class SystemOutTest {
