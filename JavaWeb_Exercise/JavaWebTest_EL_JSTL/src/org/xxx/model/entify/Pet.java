0351c74e6dba4a8f6058f5030348a223bb921681
zengge95 <sweetifan@gmail.com>
2016-04-21 22:40:03 +0800

    add a exercise

---
+++ JavaWeb_Exercise/JavaWebTest_EL_JSTL/src/org/xxx/model/entify/Pet.java
@@ -0,0 +1,14 @@
+package org.xxx.model.entify;
+
+public class Pet {
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	private String pname;
+
+}
