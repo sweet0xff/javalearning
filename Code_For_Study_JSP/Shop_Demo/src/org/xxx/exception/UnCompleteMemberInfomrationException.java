de627c98b1f451d6fd510d1b1fd8d370f7216328
zengge95 <sweetifan@gmail.com>
2016-05-02 17:49:00 +0800

    add a demo for study

---
+++ Code_For_Study_JSP/Shop_Demo/src/org/xxx/exception/UnCompleteMemberInfomrationException.java
@@ -0,0 +1,7 @@
+package cn.mldn.exception;
+
+public class UnCompleteMemberInfomrationException extends Exception {
+    public UnCompleteMemberInfomrationException(String msg) {
+        super(msg);
+    }
+}
---
+++ Code_For_Study_JSP/Shop_Demo/src/org/xxx/exception/UnEnoughAmountException.java
@@ -0,0 +1,7 @@
+package cn.mldn.exception;
+
+public class UnEnoughAmountException extends Exception {
+    public UnEnoughAmountException(String msg) {
+        super(msg);
+    }
+}
